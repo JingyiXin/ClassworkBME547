0e90b26c7e0edbd7fff5d6edc9672f0e8c0dd61d
Jenny Xin <jingyi.xin@duke.edu>
2021-08-30 12:43:46 -0400

    set up basic interface

---
+++ blood_calculator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 12:39:36 2021
+
+@author: Jenny Xin
+"""
+
+def interface():
+    print("Blood Calculator")
+    print("Make a Choice")
+    print("9 - Quit")
+    choice = input("Make a Choice")
+    print(choice)
+    return choice
+
+interface()
