f841bc60c772474341f843bcf11fb3d2022e1a47
Jenny Xin <jingyi.xin@duke.edu>
2021-09-06 12:10:02 -0400

    add output_display.py

---
+++ output_display.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 12:08:57 2021
+
+@author: Jenny Xin
+"""
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
+
