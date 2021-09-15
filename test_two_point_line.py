8592bc84b25276eb5dfe078c116ce4616bc44db5
Jenny Xin <jingyi.xin@duke.edu>
2021-09-14 23:46:46 -0400

    fixed small syntax error and checked that testing worked

--- test_two_point_line.py
+++ test_two_point_line.py
@@ -1,8 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("point1, point2, x, expected" [
-    ([1,1], [2,2], 3, 3)])
+@pytest.mark.parametrize("point1, point2, x, expected", [
+    ((1,1), (2,2), 3, 3),
+    ((0,0), (1,2), 2, 4),
+    ((0,3), (1,6), 2, 9)])
 def test_y_coordinate(point1, point2, x, expected):
     from two_point_line import y_coordinate
     answer = y_coordinate(point1, point2, x)
